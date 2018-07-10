0cbd39e11e81868798a5febc2dc7094a553fb98f
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-10 10:37:11 -0400

    login with facebook

---
+++ inicialize.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA7nfZPDJG7qHMlYAAalHWZkUcxpl7Cam4",
+    authDomain: "chat-laboratoria-60122.firebaseapp.com",
+    databaseURL: "https://chat-laboratoria-60122.firebaseio.com",
+    projectId: "chat-laboratoria-60122",
+    storageBucket: "chat-laboratoria-60122.appspot.com",
+    messagingSenderId: "344192106178"
+  };
+  firebase.initializeApp(config);

838141b09279472a04a5d3831f5caebfbb0f8e28
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-17 21:05:27 -0400

    enviar mensaje muro limpio

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -3,6 +3,8 @@ function sendMessage() {//pensar qué debe ocurrir para enviar un mensaje, desde
     const currentUser = firebase.auth().currentUser;
     const messageText = document.getElementById('message-box').value;
 
+    document.getElementById('message-box').value = "";
+
     //para tener una nueva llave en la colección messages
     const newMessageKey = firebase.database().ref().child('messages').push().key; //metodo de firebase
 

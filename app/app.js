41fd57c9ca7752ca5bbbf10edd30d30dabf95c2e
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-17 20:52:11 -0400

    Muro firebase

--- app/app.js
+++ app/app.js
@@ -1,22 +1,13 @@
 window.onload = () => {
     firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            //Si estamos logueados
-           showDashboardPage();
-            console.log("User > " + JSON.stringify(user));
+        if (user) {             //Si estamos logueados
 
-            // Acá comenzamos a escuchar por nuevos mensajes usando el evento
-            // on child_added
-           /*  firebase.database().ref('messages')
-                .limitToLast(2)
-                .on('child_added', (newMessage) => {
-                    messageContainer.innerHTML +=
-                        `<p>Nombre : ${newMessage.val().creatorName}</p>
-                         <p>${newMessage.val().text}</p>`;
-                }); */
-        } else {
-            //No estamos logueados
-           showLoginPage();
+           showDashboardPage(); //Cambiar a la pagina de dasboard
+           setupMessages();     //Dejar todo listo para mostrar mensajes.
+
+        } else {                //No estamos logueados
+            
+           showLoginPage();     //Cambiar a la pagina de login
         }
     });
 };
--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,11 +1,7 @@
 
-function inicializeChat() {
-    sendMessageDiv.style.display = "block";
-}
-
 function sendMessage() {//pensar qué debe ocurrir para enviar un mensaje, desde el usuario
     const currentUser = firebase.auth().currentUser;
-    const messageAreaText = messageArea.value;
+    const messageText = document.getElementById('message-box').value;
 
     //para tener una nueva llave en la colección messages
     const newMessageKey = firebase.database().ref().child('messages').push().key; //metodo de firebase
@@ -13,8 +9,26 @@ function sendMessage() {//pensar qué debe ocurrir para enviar un mensaje, desde
     firebase.database().ref(`messages/${newMessageKey}`).set({
         creator : currentUser.uid,
         creatorName : currentUser.displayName,
-        text : messageAreaText,
+        text : messageText,
         //profile_picture: imgurl
     }); //ref, ruta para guardar los mensajes
+}
+
+function setupMessages() {
+    firebase.database().ref('messages')
+                .limitToLast(100)
+                .on('child_added', (newMessage) => {
+                    document.getElementById("message-container").innerHTML +=
+                        `<li class="right clearfix">
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <small class=" text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>13 mins ago</small>
+                                    <strong class="pull-right primary-font">${newMessage.val().creatorName}</strong>
+                                </div>
+                                <p>${newMessage.val().text}</p>
+                            </div>
+                        </li>`;
+                });
 }
 
